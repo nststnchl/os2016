c190e3affac6a2b2530e4877c19986d03771fc3b
Anastasia Stanchul <nastyastanchul@gmail.com>
2016-03-14 20:17:57 +0300

    HW 2 DONE

--- cat/cat.c
+++ cat/cat.c
@@ -3,24 +3,27 @@
 
 int main(int argc, char** argv) {
     
-    char buff[512];
+    char buff[4096];
     int done = 0;
     int total_done = 0;
-    char text;
     while (1) {
-        done = read(0, buff, 512); 
+        done = read(STDIN_FILENO, buff, 4096); 
+        if (done == -1) {
+            fprintf(stderr, "Reading error occured");
+        }
         total_done += done;
-        text += buff;
         if (done == 0) {
             break;
         }
-    }
-    
-    while (1) {
-        done = write(1, text, 512);
-        total_done -= done;
-        if (total_done == 0) {
-            break;
+        while(1) {
+            done = write(STDOUT_FILENO, buff, done);
+            total_done -= done;
+            if (total_done == 0) {
+                break;
+            }
+            if (done == -1 || (total_done > 0 && done == 0)) {
+                fprintf(stderr, "Writing error occured");
+            }
         }
     }
 
